873149b19ac2fe151b5d185bde270e9d11b1f136
orholam <josiaha@uchicago.edu>
2021-11-05 01:20:41 -0500

    projectile

---
+++ Lab4_IntroToUnity/Assets/Scripts/BlastBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlastBehavior : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rb.MovePosition(this.transform.position +
+        this.transform.forward * 2 * Time.fixedDeltaTime);
+    }
+
+
+}
